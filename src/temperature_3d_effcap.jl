d19382ccffe366902b67349a54790b7d539e9b7a
Teslos <toniivas@gmail.com>
2022-03-17 14:50:44 +0100

    Ignore files like VTK

--- src/temperature_3d_effcap.jl
+++ src/temperature_3d_effcap.jl
@@ -8,22 +8,21 @@ using Printf
 else
     @init_parallel_stencil(Threads, Float64, 3);
 end
-
 @parallel_indices (ix,iy,iz) function updatecp!(Ci,c0,T,Lf,σ,Tm)
         if (ix>1 && ix<size(Ci,1) && iy>1 && iy<size(Ci,2) && iz>1 && iz<size(Ci,3))
-            c = (c0+Lf*exp(-(T[ix,iy,iz]-Tm)^2/(2*σ^2))
+            c = (c0+Lf*exp(-(T[ix,iy,iz]-Tm)^2/(2*σ^2)))
             Ci[ix,iy,iz] = 1/c
         end
         return
 end
-
-@parallel_indices (ix,iy,iz) function diffusion3D_step!(T2, T, Q, Ci, lam, dt, _dx, _dy, _dz, dx, dy, dz, x0, y0, z0, a,b,c)
+@parallel_indices (ix,iy,iz) function diffusion3D_step!(T2, T, Q, Lam, σ, c0, Lf, Tm, dt, _dx, _dy, _dz, dx, dy, dz, x0, y0, z0, a,b,c)
         if (ix>1 && ix<size(T2,1) && iy>1 && iy<size(T2,2) && iz>1 && iz<size(T2,3))
             source = Q*exp(-3*(( (ix*dx-x0)/a)^2+((iy*dy-y0)/b)^2+((iz*dz-z0)/c)^2))
-            T2[ix,iy,iz] = T[ix,iy,iz] + dt*(Ci[ix,iy,iz]*(
-                            - ((-lam*(T[ix+1,iy,iz] - T[ix,iy,iz])*_dx) - (-lam*(T[ix,iy,iz] - T[ix-1,iy,iz])*_dx))*_dx
-                            - ((-lam*(T[ix,iy+1,iz] - T[ix,iy,iz])*_dy) - (-lam*(T[ix,iy,iz] - T[ix,iy-1,iz])*_dy))*_dy
-                            - ((-lam*(T[ix,iy,iz+1] - T[ix,iy,iz])*_dz) - (-lam*(T[ix,iy,iz] - T[ix,iy,iz-1])*_dz))*_dz + source)
+            ci     = c0+Lf/(σ*sqrt(2π))*exp(-(T[ix,iy,iz]-Tm)^2/(2*σ^2))
+            T2[ix,iy,iz] = T[ix,iy,iz] + dt*(1/ci * (
+                            - ((-Lam[ix,iy,iz]*(T[ix+1,iy,iz] - T[ix,iy,iz])*_dx) - (-Lam[ix,iy,iz]*(T[ix,iy,iz] - T[ix-1,iy,iz])*_dx))*_dx
+                            - ((-Lam[ix,iy,iz]*(T[ix,iy+1,iz] - T[ix,iy,iz])*_dy) - (-Lam[ix,iy,iz]*(T[ix,iy,iz] - T[ix,iy-1,iz])*_dy))*_dy
+                            - ((-Lam[ix,iy,iz]*(T[ix,iy,iz+1] - T[ix,iy,iz])*_dz) - (-Lam[ix,iy,iz]*(T[ix,iy,iz] - T[ix,iy,iz-1])*_dz))*_dz + source)
                             );
         end
     return
@@ -33,13 +32,14 @@ function diffusion3D(;do_vtk=true)
 # Physics
 lam        = 30.1;                                        # Thermal conductivity
 c0         = 7860*624;                                    # Heat capacity
-Lf         = 292e+3                                       # Latent heat
 lx, ly, lz = 2e-3, 1e-3, 1e-4                             # Length of computational domain in dimension x, y and z
+Lf         = 292e+4*7860                                  # Latent heat
+Tm         = 1933
 a,  b,  c  = 0.5e-4, 0.5e-4, 5.0e-5                       # laser parameters
 v  = 1.5                                                  # laser speed
 P  = 100                                                  # power laser
-α  = 0.4
-σ  = 1e-4                                                 # absorption coefficient
+α  = 0.4                                                  # absorption coefficient
+σ  = 5
 # Numerics
 nx, ny, nz = 256, 256, 64;                               # Number of gridpoints in dimensions x, y and z
 nt         = 10000;                                      # Number of time steps
@@ -51,19 +51,19 @@ _dx, _dy, _dz = 1.0/dx, 1.0/dy, 1.0/dz;
 xc, yc, zc = Array(LinRange(dx/2, lx-dx/2, nx)), Array(LinRange(dy/2, ly-dy/2, ny)), Array(LinRange(dz/2, lz-dz/2, nz))
 
 # Array initializations
-T   = @zeros(nx, ny, nz);
-T2  = @zeros(nx, ny, nz);
-Ci  = @zeros(nx, ny, nz);
-Sl  = @zeros(nx, ny, nz);
+T   = @zeros(nx, ny, nz)
+T2  = @zeros(nx, ny, nz)
+Lam = @zeros(nx, ny, nz)
+Ci  = @zeros(nx, ny, nz)
 
 # Initial conditions
-# Ci .= 1/c0;                                              # 1/Heat capacity
-Li  = 1/Lf;
-T  .= 1.7;
-T2 .= T;                                                 # Assign also T2 to get correct boundary conditions.
-Q = 6.0*sqrt(3)*α*P/(π*sqrt(π)*a*b*c)                      # Laser flux for Goldak's heat source
+Lam .= Data.Array([if (iy > ny/2) lam else lam/10 end for ix=1:size(Lam,1), iy=1:size(Lam,2),iz=1:size(Lam,3)])                                              # conductivity
+
+T   .= 1.7;
+T2  .= T;                                                 # Assign also T2 to get correct boundary conditions.
+Q = 6.0*sqrt(3)*α*P/(π*sqrt(π)*a*b*c)                     # Laser flux for Goldak's heat source
 # Time loop
-dt   = min(dx^2,dy^2,dz^2)/lam/maximum(Ci)/8.1;          # Time step for 3D Heat diffusion
+dt   = min(dx^2,dy^2,dz^2)/lam/maximum(1/c0)/8.1;          # Time step for 3D Heat diffusion
 @printf("Choosen timestep: %g\n",dt)
 y0 = ly/2; z0 = lz-dz;
 for it = 1:nt
@@ -72,12 +72,7 @@ for it = 1:nt
     x0 = v*it*dt
 
     @hide_communication (16, 2, 2) begin
-        @parallel updatecp!(Ci,T,Lf,σ,Tm))
-        update_halo!(Ci)
-    end
-
-    @hide_communication (16, 2, 2) begin
-        @parallel diffusion3D_step!(T2, T,  Q, Ci, lam,  dt, _dx, _dy, _dz, dx,
+        @parallel diffusion3D_step!(T2, T,  Q, Lam, σ, c0, Lf, Tm, dt, _dx, _dy, _dz, dx,
                                     dy, dz, x0, y0, z0, a, b, c)
         update_halo!(T2);
     end
